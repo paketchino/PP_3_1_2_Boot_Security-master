c61b02277e14162c8bf281197f4d5085fceba1a8
paketchino <chansgorn@gmail.com>
2023-04-10 17:25:48 +0300

    pp_3_1_3_commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -0,0 +1,15 @@
+package ru.kata.spring.boot_security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+@EntityScan("ru.kata.spring.boot_security.demo")
+public class SpringBootSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+	}
+
+}
