9174dc4b874daf747d5ac7008e33b87ca50d0b00
paketchino <chansgorn@gmail.com>
2023-04-25 18:15:55 +0300

    fix

--- src/main/java/ru/kata/spring/boot_security/demo/dao/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserRepository.java
@@ -1,6 +1,5 @@
 package ru.kata.spring.boot_security.demo.dao;
 
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
